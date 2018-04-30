c75f0f05622b074748fd5f619809685310ef3927
John Walz <johnwalz97@gmail.com>
2018-04-30 18:59:54 -0400

    updates

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 require 'functions.php';
 
 // Sets DB configuration
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = 'pow54321';
-$DB_NAME = 'pathology_watch_test';
+$DB_HOST = '';
+$DB_USER = '';
+$DB_PASS = '';
+$DB_NAME = '';
 
 // Initiates DB
 $DB = db_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
